151b2094c29215bf66aa5d8a9d99e5a5dacdf259
kinntarou204 <220002kr@yse-c.net>
2024-01-17 12:04:59 +0900

    update

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -1,8 +1,32 @@
 "use client"
 
-const Input = () => {
+const className = `
+            p-3
+            my-2
+            border-2
+            border-gray-200
+            rounded
+            w-full
+            focus:outline-none
+            focus:bg-white
+            focus:border-blue-500
+`
+interface InputProps {
+    type: string;
+    value?: string;
+    placeholder?: string;
+    onChange?: (value: any) => void;
+}
+
+const Input = ({ type, value, placeholder, onChange }: InputProps) => {
+
     return (
-        <input type="text" className="border" />
+        <input
+            type={type}
+            className={className} 
+            placeholder={placeholder} 
+            onChange={(e) => { onChange && onChange(e.target.value)} }
+            />
     );
 }
 
